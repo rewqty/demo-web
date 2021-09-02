b734eaf33f32afe1f439f5bf8de0c19e82f2857c
rewqty <dmitriy.chelnokov.0000@gmail.com>
2021-09-02 12:06:49 +0500

    Add controller

---
+++ src/main/java/com/example/demoweb/controller/PostsViewController.java
@@ -0,0 +1,18 @@
+package com.example.demoweb.controller;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PostsViewController {
+    @ResponseBody
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String list() {
+        return "Здесь будет главная страница";
+    }
+
+    @ResponseBody
+    @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
+    public String single(@PathVariable("id") Long id) {
+        return "Здесь будет страница поста №" + id;
+    }
+}
