5e25594ac621ea96408ac0166dfe1728b170a7a1
rewqty <dmitriy.chelnokov.0000@gmail.com>
2021-09-15 19:53:05 +0500

    Верстка с использованием классов Bootstrap

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,12 +1,16 @@
 package com.example.demoweb.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes;
+    private Date creationDate;
 
-    public Post(String text) {
+    public Post(String text, Date creationDate) {
         this.text = text;
         this.likes = 0;
+        this.creationDate = creationDate;
     }
 
     public Integer getLikes() {
@@ -15,5 +19,9 @@ public class Post {
 
     public String getText() {
         return text;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
     }
 }
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -1,15 +1,17 @@
 package com.example.demoweb.service;
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.*;
+
+import java.util.Date;
 import java.util.*;
 
 @Service
 public class PostService {
     public ArrayList<Post> listAllPosts() {
         ArrayList<Post> posts = new ArrayList<>();
-        posts.add(new Post("Текстовый пост номер один"));
-        posts.add(new Post("Текстовый пост номер два"));
-        posts.add(new Post("Текстовый пост номер три"));
+        posts.add(new Post("Текстовый пост номер один", new Date()));
+        posts.add(new Post("Текстовый пост номер два", new Date()));
+        posts.add(new Post("Текстовый пост номер три", new Date()));
 
         return posts;
     }
