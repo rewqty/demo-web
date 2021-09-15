07f0fdfbcfaeb755a5ad2310edbfee9bbd96c02f
rewqty <dmitriy.chelnokov.0000@gmail.com>
2021-09-15 21:14:09 +0500

    База данных

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,12 +1,25 @@
 package com.example.demoweb.model;
 
+import com.fasterxml.jackson.annotation.JsonTypeId;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
 import java.util.Date;
 
-public class Post {
+@Entity
+public class Post implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String text;
     private Integer likes;
     private Date creationDate;
-    private Long id;
+
+    public Post() {
+    }
 
     public Post(Long id, String text, Date creationDate) {
         this.id = id;
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -1,25 +1,24 @@
 package com.example.demoweb.service;
-import com.example.demoweb.model.Post;
-import org.springframework.stereotype.*;
 
+import com.example.demoweb.model.Post;
+import com.example.demoweb.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.*;
 
 @Service
 public class PostService {
+    @Autowired
+    PostRepository postRepository;
 
-    private ArrayList<Post> posts = new ArrayList<>();
-    {
-        posts.add(new Post((long) posts.size(),"Текстовый пост номер один", new Date()));
-        posts.add(new Post((long) posts.size(),"Текстовый пост номер два", new Date()));
-        posts.add(new Post((long) posts.size(),"Текстовый пост номер три", new Date()));
-    }
 
-    public ArrayList<Post> listAllPosts() {
-        return posts;
+    public Iterable<Post> listAllPosts() {
+        return postRepository.findAll();
     }
 
     public void create(String text) {
-        posts.add(new Post((long) posts.size(),text, new Date()));
+        Post post = new Post(null, text, new Date());
+        postRepository.save(post);
     }
 }
