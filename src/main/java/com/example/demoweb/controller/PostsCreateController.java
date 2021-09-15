97dd1f4078a580aa53ac2d0ed1488861efa3e920
rewqty <dmitriy.chelnokov.0000@gmail.com>
2021-09-15 20:08:54 +0500

    Добавление поста

---
+++ src/main/java/com/example/demoweb/controller/PostsCreateController.java
@@ -0,0 +1,26 @@
+package com.example.demoweb.controller;
+        import com.example.demoweb.model.Post;
+        import com.example.demoweb.service.PostService;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.*;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PostsCreateController {
+    @Autowired
+    PostService postsService;
+
+    @RequestMapping(path = "/new", method = RequestMethod.GET)
+    public String create(Model model) {
+        return "create";
+    }
+
+    @RequestMapping(path = "/new", method = RequestMethod.POST)
+    public String doCreate(@ModelAttribute("text") String text) {
+        postsService.create(text);
+        return "redirect:/";
+    }
+
+
+}
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -7,12 +7,19 @@ import java.util.*;
 
 @Service
 public class PostService {
-    public ArrayList<Post> listAllPosts() {
-        ArrayList<Post> posts = new ArrayList<>();
+
+    private ArrayList<Post> posts = new ArrayList<>();
+    {
         posts.add(new Post("Текстовый пост номер один", new Date()));
         posts.add(new Post("Текстовый пост номер два", new Date()));
         posts.add(new Post("Текстовый пост номер три", new Date()));
+    }
 
+    public ArrayList<Post> listAllPosts() {
         return posts;
+    }
+
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
